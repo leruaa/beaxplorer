b021cc99995544e1d6102496f6ec5b6368f0ffdb
Aurel <aurelien.catinon@gmail.com>
2021-06-30 20:49:22 +0200

    Use shared .env

--- indexer/tests/epoch_retriever.rs
+++ indexer/tests/epoch_retriever.rs
@@ -2,8 +2,12 @@
 use indexer::epoch_retriever::EpochRetriever;
 use types::{Epoch, MainnetEthSpec};
 
+use dotenv::dotenv;
+
 #[tokio::test]
 async fn get_consolidated_epoch() {
+    dotenv().ok();
+
     let epoch_retriever = EpochRetriever::new();
 
     let consolidated_epoch = epoch_retriever.get_consolidated_epoch::<MainnetEthSpec>(Epoch::new(100)).await.unwrap();
