7c823eeb08af557fbedaec9d0875bf6be8d38b03
Aurel <aurelien.catinon@gmail.com>
2021-08-02 19:59:56 +0200

    (Web) Adding Slashings P / A and Finalized to epoch list

--- web/src/views/epoch.rs
+++ web/src/views/epoch.rs
@@ -8,9 +8,12 @@ pub struct EpochView {
     pub epoch: String,
     pub attestations_count: String,
     pub deposits_count: String,
+    pub proposer_slashings_count: String,
+    pub attester_slashings_count: String,
     pub eligible_ether: String,
     pub voted_ether: String,
     pub global_participation_percentage: String,
+    pub finalized: bool,
 }
 
 impl From<EpochModel> for EpochView {
@@ -20,8 +23,11 @@ impl From<EpochModel> for EpochView {
             attestations_count: model.attestations_count.to_string(),
             deposits_count: model.deposits_count.to_string(),
             eligible_ether: model.eligible_ether.to_ether_value(),
+            proposer_slashings_count: model.proposer_slashings_count.to_string(),
+            attester_slashings_count: model.attester_slashings_count.to_string(),
             voted_ether: model.voted_ether.to_ether_value(),
             global_participation_percentage: model.global_participation_rate.to_percentage(),
+            finalized: model.finalized.unwrap_or_default(),
         }
     }
 }
