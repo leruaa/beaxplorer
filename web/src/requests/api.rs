40844317809c534c24ceb7f6707b3e245e8e6b87
Aurélien <aurelien.catinon@gmail.com>
2021-10-18 21:36:36 +0200

    (Web) API: Returns views instead of models

--- web/src/requests/api.rs
+++ web/src/requests/api.rs
@@ -1,14 +1,25 @@
-use crate::helpers::db::NodeDbConn;
-use db::models::EpochModel;
+use std::convert::TryInto;
+
+use crate::{helpers::db::NodeDbConn, views::epoch::EpochView};
 use rocket::serde::json::Json;
+use types::MainnetEthSpec;
 
 #[get("/epochs?<page>")]
-pub async fn epochs(page: Option<i64>, db_connection: NodeDbConn) -> Json<Vec<EpochModel>> {
+pub async fn epochs(
+    page: Option<i64>,
+    db_connection: NodeDbConn,
+) -> Json<Vec<EpochView<MainnetEthSpec>>> {
     db_connection
         .run(move |c| {
-            let epochs = db::queries::epochs::get_paginated(page.unwrap_or_else(|| 1), c).unwrap();
+            let epochs = db::queries::epochs::get_paginated(page.unwrap_or_else(|| 1), c)
+                .unwrap()
+                .0
+                .into_iter()
+                .map(|e| e.try_into().ok())
+                .filter_map(|e| e)
+                .collect();
 
-            Json(epochs.0)
+            Json(epochs)
         })
         .await
 }
