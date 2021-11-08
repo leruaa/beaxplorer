4e97842d9656c5cfc6740ad326ed78b8c984ea74
Aurélien <aurelien.catinon@gmail.com>
2021-11-08 21:37:38 +0100

    (Web) Make pagination more generic

--- web/frontend/src/js/epochs.ts
+++ web/frontend/src/js/epochs.ts
@@ -1,8 +1,9 @@
-import { Grid, html } from "gridjs";
+import { html } from "gridjs";
+import { paginate } from "./pagination";
 
-let epochsList = document.getElementById("epochs-list")!;
-
-new Grid({
+paginate({
+  wrapperId: "epochs-list",
+  apiUrl: "/api/epochs",
   columns: [
     {
       name: "Epoch",
@@ -26,112 +27,17 @@ new Grid({
       formatter: (cell: any) => `${cell}%`
     }
   ],
-  server: {
-    url: '/api/epochs',
-    then: data => data.map(
-      (e: any) => [
-        e.epoch,
-        e.ago,
-        e.attestations_count,
-        e.deposits_count,
-        `${e.proposer_slashings_count} / ${e.attester_slashings_count}`,
-        `${e.finalized ? "Yes" : "No"}`,
-        e.eligible_ether,
-        e.voted_ether,
-        e.global_participation_percentage
-      ]
-    ),
-    total: (data) => parseInt(epochsList.dataset.pageCount!) * 10
-  },
-  pagination: {
-    enabled: true,
-    summary: false,
-    limit: 10,
-    server: {
-      url: (prev, page, limit) => `${prev}?page=${page + 1}`
-    }
-  }
-}).render(epochsList);
-
-/*
-paginate("epochs",
-  (row) => {
-    row
-      .append("td")
-      .append("a")
-      .attr("href", (d: any) => `/epoch/${d.epoch}`)
-      .text((d: any) => d.epoch);
-
-    row
-      .append("td")
-      .text((d: any) => d.ago);
-
-    row
-      .append("td")
-      .text((d: any) => d.attestations_count);
-
-    row
-      .append("td")
-      .text((d: any) => d.deposits_count);
-
-    row
-      .append("td")
-      .text((d: any) => `${d.proposer_slashings_count} / ${d.attester_slashings_count}`);
-
-    row
-      .append("td")
-      .text((d: any) => d.finalized ? "Yes" : "No");
-
-    row
-      .append("td")
-      .text((d: any) => `${d.eligible_ether} ETH`);
-
-    row
-      .append("td")
-      .text((d: any) => `${d.voted_ether} ETH`);
-
-    row
-      .append("td")
-      .text((d: any) => `${d.global_participation_percentage}%`);
-
-  },
-  (row) => {
-    row
-      .select("td:nth-child(1) > a")
-      .attr("href", (d: any) => `/epoch/${d.epoch}`)
-      .text((d: any) => d.epoch);
-
-    row
-      .select("td:nth-child(2)")
-      .text((d: any) => d.ago);
-
-    row
-      .select("td:nth-child(3)")
-      .text((d: any) => d.attestations_count);
-
-    row
-      .select("td:nth-child(4)")
-      .text((d: any) => d.deposits_count);
-
-    row
-      .select("td:nth-child(5)")
-      .text((d: any) => `${d.proposer_slashings_count} / ${d.attester_slashings_count}`);
-
-    row
-      .select("td:nth-child(6)")
-      .text((d: any) => d.finalized ? "Yes" : "No");
-
-    row
-      .select("td:nth-child(7)")
-      .text((d: any) => `${d.eligible_ether} ETH`);
-
-    row
-      .select("td:nth-child(8)")
-      .text((d: any) => `${d.voted_ether} ETH`);
-
-    row
-      .select("td:nth-child(9)")
-      .text((d: any) => `${d.global_participation_percentage}%`);
-  }
-);
-*/
+  dataMapping: (data: any) => data.map(
+    (e: any) => [
+      e.epoch,
+      e.ago,
+      e.attestations_count,
+      e.deposits_count,
+      `${e.proposer_slashings_count} / ${e.attester_slashings_count}`,
+      `${e.finalized ? "Yes" : "No"}`,
+      e.eligible_ether,
+      e.voted_ether,
+      e.global_participation_percentage
+    ]
+  )
+});
--- web/frontend/src/js/pagination.ts
+++ web/frontend/src/js/pagination.ts
@@ -1,45 +1,30 @@
-import * as d3 from 'd3';
+import { Grid } from "gridjs";
 
-export interface Refresh {
-  (rows: d3.Selection<d3.BaseType | HTMLTableRowElement, unknown, d3.BaseType, unknown>): void;
+export interface PaginationSettings {
+  wrapperId: string,
+  columns: any[],
+  apiUrl: string,
+  dataMapping: (data: any) => any[][]
 }
 
-export function paginate(type: string, refresh: Refresh) {
-  const firstPage = d3.select("#first-page");
-  const previousPage = d3.select("#previous-page");
-  const currentPage = d3.select("#current-page");
-  const pageCount = d3.select("#page-count");
-  const nextPage = d3.select("#next-page");
-  const lastPage = d3.select("#last-page");
+export function paginate(settings: PaginationSettings) {
+  let wrapper = document.getElementById(settings.wrapperId)!;
 
-  let currentPageIndex = 1;
-  let totalPageCount = parseInt(pageCount.attr("data-page-count"));
-
-  const update = (newPageIndex: number) => {
-    fetch(`/api/${type}?page=${newPageIndex}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-
-        let rows = d3.select("#" + type).selectAll("tbody > tr")
-          .data(json)
-          .join("tr");
-
-        refresh(rows);
-
-        firstPage.attr("disabled", newPageIndex == 1 ? "disabled" : null);
-        previousPage.attr("disabled", newPageIndex == 1 ? "disabled" : null);
-        currentPage.attr("value", newPageIndex);
-        nextPage.attr("disabled", newPageIndex == totalPageCount ? "disabled" : null);
-        lastPage.attr("disabled", newPageIndex == totalPageCount ? "disabled" : null);
-        currentPageIndex = newPageIndex;
-      });
-  }
-
-  firstPage.on("click", () => update(1));
-  nextPage.on("click", () => update(currentPageIndex + 1));
-  previousPage.on("click", () => update(currentPageIndex - 1));
-  lastPage.on("click", () => update(totalPageCount));
+  new Grid({
+    columns: settings.columns,
+    server: {
+      url: settings.apiUrl,
+      then: settings.dataMapping,
+      total: (data) => parseInt(wrapper.dataset.pageCount!) * 10
+    },
+    pagination: {
+      enabled: true,
+      summary: false,
+      limit: 10,
+      server: {
+        url: (prev, page, limit) => `${prev}?page=${page + 1}`
+      }
+    }
+  }).render(wrapper);
 }
 
