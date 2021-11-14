b20c44c1223d14eb7e375b926887c5235dbe891a
Aurélien <aurelien.catinon@gmail.com>
2021-11-14 19:50:20 +0100

    (Web) Then order by epoch

--- web/src/controllers/epochs.rs
+++ web/src/controllers/epochs.rs
@@ -15,40 +15,44 @@ pub fn get_paginated<'a>(
         sort.as_ref().map_or("epoch", String::as_str),
         direction.as_ref().map_or("asc", String::as_str),
     ) {
-        ("epoch", "desc") | ("ago", "desc") => query.order(columns::epoch.desc()),
+        ("epoch", "desc") | ("ago", "desc") => query.order_by(columns::epoch.desc()),
 
-        ("attestations_count", "desc") => query.order(columns::attestations_count.desc()),
-        ("attestations_count", _) => query.order(columns::attestations_count.asc()),
+        ("attestations_count", "desc") => query.order_by(columns::attestations_count.desc()),
+        ("attestations_count", _) => query.order_by(columns::attestations_count.asc()),
 
-        ("deposits_count", "desc") => query.order(columns::deposits_count.desc()),
-        ("deposits_count", _) => query.order(columns::deposits_count.asc()),
+        ("deposits_count", "desc") => query.order_by(columns::deposits_count.desc()),
+        ("deposits_count", _) => query.order_by(columns::deposits_count.asc()),
 
         ("proposer_slashings_count", "desc") => {
-            query.order(columns::proposer_slashings_count.desc())
+            query.order_by(columns::proposer_slashings_count.desc())
         }
-        ("proposer_slashings_count", _) => query.order(columns::proposer_slashings_count.asc()),
+        ("proposer_slashings_count", _) => query.order_by(columns::proposer_slashings_count.asc()),
 
         ("attester_slashings_count", "desc") => {
-            query.order(columns::attester_slashings_count.desc())
+            query.order_by(columns::attester_slashings_count.desc())
         }
-        ("attester_slashings_count", _) => query.order(columns::attester_slashings_count.asc()),
+        ("attester_slashings_count", _) => query.order_by(columns::attester_slashings_count.asc()),
 
-        ("finalized", "desc") => query.order(columns::finalized.desc()),
-        ("finalized", _) => query.order(columns::finalized.asc()),
+        ("finalized", "desc") => query.order_by(columns::finalized.desc()),
+        ("finalized", _) => query.order_by(columns::finalized.asc()),
 
-        ("eligible_ether", "desc") => query.order(columns::eligible_ether.desc()),
-        ("eligible_ether", _) => query.order(columns::eligible_ether.asc()),
+        ("eligible_ether", "desc") => query.order_by(columns::eligible_ether.desc()),
+        ("eligible_ether", _) => query.order_by(columns::eligible_ether.asc()),
 
-        ("voted_ether", "desc") => query.order(columns::voted_ether.desc()),
-        ("voted_ether", _) => query.order(columns::voted_ether.asc()),
+        ("voted_ether", "desc") => query.order_by(columns::voted_ether.desc()),
+        ("voted_ether", _) => query.order_by(columns::voted_ether.asc()),
 
         ("global_participation_rate", "desc") => {
-            query.order(columns::global_participation_rate.desc())
+            query.order_by(columns::global_participation_rate.desc())
         }
-        ("global_participation_rate", _) => query.order(columns::global_participation_rate.asc()),
+        ("global_participation_rate", _) => {
+            query.order_by(columns::global_participation_rate.asc())
+        }
 
-        (_, _) => query.order(columns::epoch.asc()),
+        (_, _) => query.order_by(columns::epoch.asc()),
     };
+
+    query = query.then_order_by(columns::epoch.asc());
 
     query.paginate(page).load_and_count_pages(connection)
 }
