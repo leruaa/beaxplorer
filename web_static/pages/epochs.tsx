2546ff8ca12e0c0b19590bb4ed20e284e9e8d1b2
Aurélien <aurelien.catinon@gmail.com>
2021-12-01 22:04:48 +0100

    (Web) WIP: init data tables

--- web_static/pages/epochs.tsx
+++ web_static/pages/epochs.tsx
@@ -1,16 +1,69 @@
+import { useMemo } from "react";
+import DataTable from "../components/data-table";
 import Breadcrumb from "../components/breadcrumb";
 
 export default () => {
-    return (
-      <>
-        <Breadcrumb breadcrumb={{ parts: [{ text: "Epochs", icon: "clock" }] }} />
-        <section className="container mx-auto">
-            <div className="tabular-data">
-            <p>Showing epochs</p>
-            <div id="epochs-list"></div>
-            </div>
-        </section>
-      </>
-    )
-  }
+
+  const columns = [
+    {
+      accessor: "epoch",
+      Header: "Epoch"
+    },
+    {
+      accessor: "ago",
+      Header: "Time"
+    },
+    {
+      accessor: "attestations_count",
+      Header: "Attestations"
+    },
+    {
+      accessor: "deposits_count",
+      Header: "Deposits",
+    },
+    {
+      accessor: "attester_slashings_count",
+      Header: "Slashings P / A",
+    },
+    {
+      accessor: "finalized",
+      Header: "Finalized"
+    },
+    {
+      accessor: "eligible_ether",
+      Header: "Eligible"
+    },
+    {
+      accessor: "voted_ether",
+      Header: "Voted"
+    },
+    {
+      accessor: "global_participation_rate",
+      Header: "Rate"
+    }
+  ];
+
+  const data = [
+    {
+      epoch: 1,
+      attestations_count: 965
+    },
+    {
+      epoch: 2,
+      attestations_count: 486
+    },
+  ]
+
+  return (
+    <>
+      <Breadcrumb breadcrumb={{ parts: [{ text: "Epochs", icon: "clock" }] }} />
+      <section className="container mx-auto">
+        <div className="tabular-data">
+          <p>Showing epochs</p>
+          <DataTable columns={useMemo(() => columns, [])} data={useMemo(() => data, [])}/>
+        </div>
+      </section>
+    </>
+  )
+}
   
