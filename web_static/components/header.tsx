4f4663b62839f9ee7d99b7d825f0067e37f745d0
Aurélien <aurelien.catinon@gmail.com>
2021-12-01 19:51:53 +0100

    (Web) Simplify header component

--- web_static/components/header.tsx
+++ web_static/components/header.tsx
@@ -1,12 +1,16 @@
-import Nav from "./nav";
-
 export default () => {
   return (
     <>
       <header>
-        <Nav />
+        <nav className="container mx-auto flex flex-row text-lg">
+          <h1 className="text-xl py-2"><a href="/">Beacon explorer</a></h1>
+          <ol className="flex flex-row flex-grow justify-end">
+            <li className="p-2"><a href="/epochs"><i className="icon outline-clock" /> Epochs</a></li>
+            <li className="p-2"><a href="/blocks"><i className="icon outline-cube" /> Blocks</a></li>
+            <li className="p-2"><a href="/validators"><i className="icon outline-users" /> Validators</a></li>
+          </ol>
+        </nav>
       </header>
-
     </>
   )
 }
