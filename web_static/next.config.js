98f7dbec802b9131fd535b99b5e0614f23a2e0f1
Aurélien <aurelien.catinon@gmail.com>
2021-12-10 21:58:09 +0100

    (Web) Config wasm

---
+++ web_static/next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  experimental: { esmExternals: true },
+  webpack(config, { isServer, dev }) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true
+    };
+
+    // In prod mode and in the server bundle (the place where this "chunks" bug
+    // appears), use the client static directory for the same .wasm bundle
+    config.output.webassemblyModuleFilename =
+      isServer && !dev ? "../static/wasm/[id].wasm" : "static/wasm/[id].wasm";
+
+    // Ensure the filename for the .wasm bundle is the same on both the client
+    // and the server (as in any other mode the ID's won't match)
+    config.optimization.moduleIds = "named";
+    return config;
+  }
+}
