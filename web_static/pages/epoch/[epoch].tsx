f93fd7df95cd836254a918ef863c38ae18ab0324
Aurélien <aurelien.catinon@gmail.com>
2021-12-10 21:58:31 +0100

    (Web) Init epoch detail page

---
+++ web_static/pages/epoch/[epoch].tsx
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router'
+import Breadcrumb from "../../components/breadcrumb";
+
+export async function getServerSideProps(context) {
+  const wasmModule = await import('../../pkg');
+
+  return {
+    props: await wasmModule.get_epoch(context.params.epoch)
+  }
+}
+
+export default (props) => { 
+  const router = useRouter()
+  const { epoch } = router.query
+
+  return (
+    <>
+      <Breadcrumb breadcrumb={{ parts: [{ text: "Epochs", icon: "clock" }] }} />
+      <section className="container mx-auto">
+        <div className="tabular-data">
+          <p>Showing epoch</p>
+
+          <dl>
+            <dt>Epoch</dt>
+            <dd>{props.epoch} ({epoch})</dd>
+            <dt>Finalized</dt>
+            <dd>
+              {props.finalized
+                ? <span>Yes</span>
+                : <span>No</span>
+              }
+            </dd>
+            <dt>Time</dt>
+            <dd>{props.time} ({props.ago})</dd>
+            <dt>Attestations</dt>
+            <dd>{props.attestations_count}</dd>
+            <dt>Deposits</dt>
+            <dd>{props.deposits_count}</dd>
+            <dt>Slashings P / A</dt>
+            <dd>{props.proposer_slashings_count} / {props.attester_slashings_count}</dd>
+            <dt>Voting Participation</dt>
+            <dd>{props.eligible_ether} ETH of {props.voted_ether} ETH ({props.global_participation_percentage}%)</dd>
+          </dl>
+        </div>
+      </section>
+    </>
+  )
+
+}
