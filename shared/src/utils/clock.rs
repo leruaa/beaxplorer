c72e5c05d77c3184af5571330bb37ef1c2fede2f
Aurélien <aurelien.catinon@gmail.com>
2021-12-22 22:07:09 +0100

    Allow to retreive a slot timestamp

--- shared/src/utils/clock.rs
+++ shared/src/utils/clock.rs
@@ -23,12 +23,12 @@ impl Clock {
         self.clock.start_of(slot)
     }
 
+    pub fn timestamp(&self, slot: Slot) -> Option<u64> {
+        self.start_of(slot).map(|duration| duration.as_secs())
+    }
+
     pub fn format(&self, slot: Slot) -> String {
-        let timestamp = self
-            .clock
-            .start_of(slot)
-            .unwrap_or(Duration::new(0, 0))
-            .as_secs();
+        let timestamp = self.timestamp(slot).unwrap_or(0);
         let date = Utc.timestamp(timestamp as i64, 0);
 
         date.format("%a, %e %b %Y %r %Z").to_string()
