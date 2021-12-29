c21336bb354b0d5879bd04898481544ec2867db4
Aurélien <aurelien.catinon@gmail.com>
2021-12-29 21:38:41 +0100

    (Indexer) Create dirs if they don't exists

--- indexer_cli/src/db_to_files.rs
+++ indexer_cli/src/db_to_files.rs
@@ -1,10 +1,12 @@
-use std::sync::Arc;
+use std::{fs, sync::Arc};
 
 use db::{ConnectionManager, PgConnection, Pool};
 use indexer::db_to_files_indexer::Indexer;
 
 pub fn process(db_pool: Arc<Pool<ConnectionManager<PgConnection>>>) -> () {
     let indexer = Indexer {};
+
+    fs::create_dir_all("../web_static/public/data/epochs/s/attestations_count/");
 
     indexer.persist_epochs(&db_pool);
 }
