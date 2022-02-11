7324f52705bd385a2b41d9bf3201b651b22c644a
acatinon <aurelien.catinon@gmail.com>
2022-02-11 09:29:01 +0000

    Adding indexing feature

--- types/src/attestation.rs
+++ types/src/attestation.rs
@@ -1,8 +1,3 @@
-use lighthouse_types::AggregateSignature;
-use lighthouse_types::Attestation;
-use lighthouse_types::Checkpoint;
-use lighthouse_types::EthSpec;
-use lighthouse_types::Hash256;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -13,23 +8,24 @@ use crate::path::AsPath;
 pub struct AttestationModel {
     pub aggregation_bits: Vec<bool>,
     pub committee_index: u64,
-    pub beacon_block_root: Hash256,
-    pub source: Checkpoint,
-    pub target: Checkpoint,
-    pub signature: AggregateSignature,
+    pub beacon_block_root: String,
+    pub source: u64,
+    pub target: u64,
+    pub signature: String,
 }
 
 pub type AttestationsModelWithId = ModelWithId<Vec<AttestationModel>>;
 
-impl<T: EthSpec> From<&Attestation<T>> for AttestationModel {
-    fn from(attestation: &Attestation<T>) -> Self {
+#[cfg(feature = "indexing")]
+impl<T: lighthouse_types::EthSpec> From<&lighthouse_types::Attestation<T>> for AttestationModel {
+    fn from(attestation: &lighthouse_types::Attestation<T>) -> Self {
         AttestationModel {
             aggregation_bits: attestation.aggregation_bits.iter().collect(),
             committee_index: attestation.data.index,
-            beacon_block_root: attestation.data.beacon_block_root,
-            source: attestation.data.source,
-            target: attestation.data.target,
-            signature: attestation.signature.clone(),
+            beacon_block_root: attestation.data.beacon_block_root.to_string(),
+            source: attestation.data.source.epoch.as_u64(),
+            target: attestation.data.target.epoch.as_u64(),
+            signature: attestation.signature.to_string(),
         }
     }
 }
