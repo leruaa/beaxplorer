490ee11ef32a42e3215c29655629f1ca027c86a6
aurel <aurelien.catinon@gmail.com>
2023-02-28 22:28:16 +0000

    Cleanup persistable_field macro

--- indexer_macro/src/lib.rs
+++ indexer_macro/src/lib.rs
@@ -1,38 +1,10 @@
-use convert_case::{Case, Casing};
 use darling::FromDeriveInput;
-use persistable::{Index, PersistableOpts, SortableField};
-use persistable_field::{Input, PersistableFieldAttributeMetadata};
+use persistable::{Index, PersistableOpts};
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::parse_macro_input;
+
 mod persistable;
-mod persistable_field;
-
-#[proc_macro_attribute]
-pub fn persistable_field(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attribute_meta = parse_macro_input!(attr as PersistableFieldAttributeMetadata);
-    let input = parse_macro_input!(item as Input);
-
-    let model_type = attribute_meta.model_type;
-    let field_name = attribute_meta.field_name;
-    let field_type = attribute_meta.field_type;
-    let field_struct = input.field_struct;
-
-    let expanded = quote! {
-        pub struct #field_struct;
-        // The generated impl.
-        impl PersistableField<#model_type> for #field_struct {
-            type Field = #field_type;
-            const FIELD_NAME: &'static str = stringify!(#field_name);
-
-            fn get_value(value: &#model_type) -> Orderable<Self::Field> {
-                (value.id, value.model.#field_name.clone()).into()
-            }
-        }
-    };
-
-    TokenStream::from(expanded)
-}
 
 #[proc_macro_derive(Persistable, attributes(persistable, sortable))]
 pub fn persistable(input: TokenStream) -> TokenStream {
