88c7f50fa26b2dee74e8b089beb4fe41945ec3a0
aurel <aurelien.catinon@gmail.com>
2023-03-05 21:48:29 +0000

    Use next/link for header

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default () => {
   return (
     <>
@@ -5,9 +7,9 @@ export default () => {
         <nav className="container mx-auto flex flex-row text-lg">
           <h1 className="text-xl py-2"><a href="/">Beacon explorer</a></h1>
           <ol className="flex flex-row flex-grow justify-end">
-            <li className="p-2"><a href="/epochs"><i className="icon outline-clock" /> Epochs</a></li>
-            <li className="p-2"><a href="/blocks"><i className="icon outline-cube" /> Blocks</a></li>
-            <li className="p-2"><a href="/validators"><i className="icon outline-users" /> Validators</a></li>
+            <li className="p-2"><Link href="/epochs"><a><i className="icon outline-clock" /> Epochs</a></Link></li>
+            <li className="p-2"><Link href="/blocks"><a><i className="icon outline-cube" /> Blocks</a></Link></li>
+            <li className="p-2"><Link href="/validators"><a><i className="icon outline-users" /> Validators</a></Link></li>
           </ol>
         </nav>
       </header>
