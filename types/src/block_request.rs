742bd811f5b498a779ab8c0eba7d2b734e242a1a
aurel <aurelien.catinon@gmail.com>
2023-03-10 21:33:45 +0000

    Add Meta for  block requests

--- types/src/block_request.rs
+++ types/src/block_request.rs
@@ -1,7 +1,9 @@
-use indexer_macro::{Persistable, ToPathWithId};
+use indexer_macro::{Persistable, ToPath, ToPathWithId};
 use serde::{Deserialize, Serialize};
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::model::ModelWithId;
+use crate::{meta::Meta, model::ModelWithId};
 
 #[derive(Persistable, ToPathWithId, Serialize, Deserialize, Debug, Clone)]
 #[persistable(index = "model")]
@@ -11,4 +13,23 @@ pub struct BlockRequestModel {
     pub failed_count: u64,
     pub not_found_count: u64,
     pub state: String,
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+#[derive(ToPath, Serialize, Deserialize, Debug, Clone)]
+#[to_path(prefix = "/block_requests/meta")]
+pub struct BlockRequestsMeta {
+    pub count: usize,
+}
+
+impl BlockRequestsMeta {
+    pub fn new(count: usize) -> Self {
+        BlockRequestsMeta { count }
+    }
+}
+
+impl Meta for BlockRequestsMeta {
+    fn count(&self) -> usize {
+        self.count
+    }
 }
