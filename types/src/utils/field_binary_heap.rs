a3560f62f46bf0dc364d3f4d5bda588d4f5e4814
leruaa <aurelien.catinon@gmail.com>
2023-03-14 16:48:24 +0100

    Generalize Orderable Id

--- types/src/utils/field_binary_heap.rs
+++ types/src/utils/field_binary_heap.rs
@@ -1,35 +1,35 @@
 use std::{collections::BinaryHeap, fs::File, io::BufWriter};
 
 use rmp_serde::Serializer;
-use serde::Serialize;
+use serde::{de::DeserializeOwned, Serialize};
 
 use super::Orderable;
 
 #[derive(Default)]
-pub struct FieldBinaryHeap<F: Ord> {
-    inner: BinaryHeap<Orderable<F>>,
+pub struct FieldBinaryHeap<Id: Clone + Ord + Serialize + DeserializeOwned, F: Ord> {
+    inner: BinaryHeap<Orderable<Id, F>>,
 }
 
-impl<F: Ord> FieldBinaryHeap<F> {
+impl<Id: Clone + Ord + Serialize + DeserializeOwned, F: Ord> FieldBinaryHeap<Id, F> {
     pub fn new() -> Self {
         FieldBinaryHeap {
             inner: BinaryHeap::new(),
         }
     }
 
-    pub fn from_orderables(iter: impl Iterator<Item = Orderable<F>>) -> Self {
+    pub fn from_orderables(iter: impl Iterator<Item = Orderable<Id, F>>) -> Self {
         let mut heap = Self::new();
         heap.inner.extend(iter);
         heap
     }
 
-    pub fn push(&mut self, item: Orderable<F>) {
+    pub fn push(&mut self, item: Orderable<Id, F>) {
         self.inner.push(item);
     }
 
     pub fn persist(self, base_dir: &str, field_name: &str) -> Result<(), String> {
         for (i, chunk) in self.inner.into_sorted_vec().chunks(10).enumerate() {
-            let indexes: Vec<u64> = chunk.iter().map(|x| x.id).collect();
+            let indexes: Vec<Id> = chunk.iter().map(|x| x.id.clone()).collect();
             let path = format!("{}/s/{}/{}.msg", base_dir, field_name, i + 1);
             let mut f = BufWriter::new(
                 File::create(&path).map_err(|_| format!("File not found: {}", path))?,
--- types/src/utils/orderable.rs
+++ types/src/utils/orderable.rs
@@ -1,12 +1,12 @@
 use std::cmp::Ordering;
 
 #[derive(Eq)]
-pub struct Orderable<O: Ord> {
-    pub id: u64,
+pub struct Orderable<Id: Ord, O: Ord> {
+    pub id: Id,
     pub ordering: O,
 }
 
-impl<O: Ord> Ord for Orderable<O> {
+impl<Id: Ord, O: Ord> Ord for Orderable<Id, O> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         match self.ordering.cmp(&other.ordering) {
             Ordering::Equal => self.id.cmp(&other.id),
@@ -15,20 +15,20 @@ impl<O: Ord> Ord for Orderable<O> {
     }
 }
 
-impl<O: Ord> PartialOrd for Orderable<O> {
+impl<Id: Ord, O: Ord> PartialOrd for Orderable<Id, O> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<O: Ord> PartialEq for Orderable<O> {
+impl<Id: Ord, O: Ord> PartialEq for Orderable<Id, O> {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
 }
 
-impl<O: Ord> From<(u64, O)> for Orderable<O> {
-    fn from(from: (u64, O)) -> Self {
+impl<Id: Ord, O: Ord> From<(Id, O)> for Orderable<Id, O> {
+    fn from(from: (Id, O)) -> Self {
         Orderable {
             id: from.0,
             ordering: from.1,
