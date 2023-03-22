862be174cadbac7bca59bffd8e0ccb98704d0f40
aurel <aurelien.catinon@gmail.com>
2023-03-22 07:37:08 +0000

    Handle missing NetworkEvents

--- indexer/src/network/event.rs
+++ indexer/src/network/event.rs
@@ -3,15 +3,17 @@ use std::sync::Arc;
 use lighthouse_network::PeerId;
 use lighthouse_types::{EthSpec, Hash256, SignedBeaconBlock, Slot};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum NetworkEvent<E: EthSpec> {
     PeerConnected(PeerId),
     PeerDisconnected(PeerId),
     RangeRequestSuccedeed(u64),
     RangeRequestFailed(u64),
     BlockRequestFailed(Hash256, PeerId),
-    ProposedBlock(Arc<SignedBeaconBlock<E>>),
+    ProposedBlock(Arc<SignedBeaconBlock<E>>, Slot),
     OrphanedBlock(Arc<SignedBeaconBlock<E>>),
     MissedBlock(Slot),
     UnknownBlockRoot(Slot, Hash256),
+    BlockRootFound(Hash256, PeerId),
+    BlockRootNotFound(Hash256),
 }
--- indexer/src/network/event_adapter.rs
+++ indexer/src/network/event_adapter.rs
@@ -1,68 +1,58 @@
-use std::{
-    collections::HashSet,
-    iter::once,
-    pin::Pin,
-    sync::Arc,
-    task::{Context, Poll},
-};
+use std::{iter::once, sync::Arc};
 
-use futures::Stream;
 use itertools::Itertools;
 use lighthouse_network::{NetworkEvent as LighthouseNetworkEvent, Response};
-use lighthouse_types::{EthSpec, Hash256, SignedBeaconBlock, Slot};
-use tokio::sync::{
-    mpsc::UnboundedReceiver,
-    mpsc::{unbounded_channel, UnboundedSender},
-};
+use lighthouse_types::{EthSpec, SignedBeaconBlock, Slot};
+use tokio::sync::broadcast::{channel, Receiver, Sender};
 
-use super::{augmented_network_service::RequestId, event::NetworkEvent};
+use super::{augmented_network_service::RequestId, block_db::BlockDb, event::NetworkEvent};
 
 pub struct EventAdapter<E: EthSpec> {
-    network_event_send: UnboundedSender<NetworkEvent<E>>,
-    network_event_recv: UnboundedReceiver<NetworkEvent<E>>,
-    latest_slot: Option<Slot>,
-    proposed_block_roots: HashSet<Hash256>,
+    network_event_send: Sender<NetworkEvent<E>>,
+    block_db: Arc<BlockDb>,
 }
 
 impl<E: EthSpec> EventAdapter<E> {
-    pub fn new() -> Self {
-        let (network_event_send, network_event_recv) = unbounded_channel();
+    pub fn new(block_db: Arc<BlockDb>) -> Self {
+        let (network_event_send, _) = channel(16);
 
         Self {
             network_event_send,
-            network_event_recv,
-            latest_slot: None,
-            proposed_block_roots: HashSet::new(),
+            block_db,
         }
     }
 
     pub fn handle(&mut self, network_event: LighthouseNetworkEvent<RequestId, E>) {
         match network_event {
-            LighthouseNetworkEvent::PeerConnectedOutgoing(peer_id) => self
-                .network_event_send
-                .send(NetworkEvent::PeerConnected(peer_id))
-                .unwrap(),
+            LighthouseNetworkEvent::PeerConnectedOutgoing(peer_id) => {
+                let _ = self
+                    .network_event_send
+                    .send(NetworkEvent::PeerConnected(peer_id));
+            }
 
-            LighthouseNetworkEvent::PeerDisconnected(peer_id) => self
-                .network_event_send
-                .send(NetworkEvent::PeerDisconnected(peer_id))
-                .unwrap(),
+            LighthouseNetworkEvent::PeerDisconnected(peer_id) => {
+                let _ = self
+                    .network_event_send
+                    .send(NetworkEvent::PeerDisconnected(peer_id));
+            }
 
             LighthouseNetworkEvent::RPCFailed {
                 id: RequestId::Range(id),
                 ..
-            } => self
-                .network_event_send
-                .send(NetworkEvent::RangeRequestFailed(id))
-                .unwrap(),
+            } => {
+                let _ = self
+                    .network_event_send
+                    .send(NetworkEvent::RangeRequestFailed(id));
+            }
 
             LighthouseNetworkEvent::RPCFailed {
                 id: RequestId::Block(root),
                 peer_id,
-            } => self
-                .network_event_send
-                .send(NetworkEvent::BlockRequestFailed(root, peer_id))
-                .unwrap(),
+            } => {
+                let _ = self
+                    .network_event_send
+                    .send(NetworkEvent::BlockRequestFailed(root, peer_id));
+            }
 
             LighthouseNetworkEvent::ResponseReceived {
                 id: RequestId::Range(start_slot),
@@ -70,8 +60,6 @@ impl<E: EthSpec> EventAdapter<E> {
                 ..
             } => {
                 if let Some(block) = block {
-                    self.proposed_block_roots.insert(block.canonical_root());
-
                     block
                         .message()
                         .body()
@@ -79,20 +67,43 @@ impl<E: EthSpec> EventAdapter<E> {
                         .iter()
                         .map(|a| (a.data.slot, a.data.beacon_block_root))
                         .dedup()
-                        .filter(|(_, r)| !self.proposed_block_roots.contains(r))
+                        .filter(|(_, r)| !self.block_db.contain_block_root(r))
                         .for_each(|(slot, root)| {
                             self.network_event_send
                                 .send(NetworkEvent::UnknownBlockRoot(slot, root))
-                                .unwrap()
+                                .unwrap();
                         });
 
-                    self.new_blocks(block)
-                        .for_each(|event| self.network_event_send.send(event).unwrap())
+                    self.new_blocks(block).for_each(|event| {
+                        let _ = self.network_event_send.send(event);
+                    })
                 } else {
                     // A block range response has finished
                     self.network_event_send
                         .send(NetworkEvent::RangeRequestSuccedeed(start_slot))
-                        .unwrap()
+                        .unwrap();
+                }
+            }
+
+            LighthouseNetworkEvent::ResponseReceived {
+                peer_id,
+                id: RequestId::Block(root),
+                response: Response::BlocksByRoot(block),
+            } => {
+                if self.block_db.block_by_root_request_exists(&root) {
+                    if let Some(block) = block {
+                        self.network_event_send
+                            .send(NetworkEvent::OrphanedBlock(block))
+                            .unwrap();
+
+                        self.network_event_send
+                            .send(NetworkEvent::BlockRootFound(root, peer_id))
+                            .unwrap();
+                    } else {
+                        self.network_event_send
+                            .send(NetworkEvent::BlockRootNotFound(root))
+                            .unwrap();
+                    }
                 }
             }
 
@@ -100,24 +111,20 @@ impl<E: EthSpec> EventAdapter<E> {
         };
     }
 
+    pub fn receiver(&self) -> Receiver<NetworkEvent<E>> {
+        self.network_event_send.subscribe()
+    }
+
     fn new_blocks(
         &mut self,
         block: Arc<SignedBeaconBlock<E>>,
     ) -> impl Iterator<Item = NetworkEvent<E>> {
-        let previous_latest_slot = self.latest_slot.unwrap_or_else(|| Slot::new(0));
-        self.latest_slot = Some(block.message().slot());
+        let previous_latest_slot = self.block_db.latest_slot().unwrap_or_else(|| Slot::new(0));
+        let latest_slot = block.message().slot();
 
-        (previous_latest_slot.as_u64()..self.latest_slot.unwrap_or_else(|| Slot::new(0)).as_u64())
+        (previous_latest_slot.as_u64()..latest_slot.as_u64())
             .map(Slot::new)
             .map(NetworkEvent::MissedBlock)
-            .chain(once(NetworkEvent::ProposedBlock(block)))
-    }
-}
-
-impl<E: EthSpec> Stream for EventAdapter<E> {
-    type Item = NetworkEvent<E>;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.get_mut().network_event_recv.poll_recv(cx)
+            .chain(once(NetworkEvent::ProposedBlock(block, latest_slot)))
     }
 }
