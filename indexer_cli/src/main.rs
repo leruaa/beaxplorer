1888bf1007f9fa9cdda6643f39d22373035f3b72
leruaa <aurelien.catinon@gmail.com>
2023-04-04 13:14:40 +0200

    Allow to set log level with RUST_LOG

--- indexer_cli/src/main.rs
+++ indexer_cli/src/main.rs
@@ -4,6 +4,7 @@ use clap::StructOpt;
 use cli::Commands;
 use dotenv::dotenv;
 use indexer::launcher;
+use tracing_subscriber::EnvFilter;
 
 use crate::cli::Cli;
 
@@ -12,9 +13,11 @@ mod cli;
 
 fn main() {
     dotenv().ok();
+
     tracing_subscriber::fmt()
         .compact()
         .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
         .init();
 
     let cli = Cli::parse();
