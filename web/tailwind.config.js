f57ba3384f9d93ef9eb5fd34529d08286d7ac253
aurel <aurelien.catinon@gmail.com>
2023-04-15 07:49:47 +0000

    Update Tailwind to 3.3

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       padding: {
@@ -12,9 +10,6 @@ module.exports = {
         '2xl': '6rem',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
