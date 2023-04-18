cbf72a8b15eb06c4a5d2bd658c406d27145d62ee
aurel <aurelien.catinon@gmail.com>
2023-04-18 06:19:13 +0000

    Cleanup epoch extended view

--- types/src/epoch.rs
+++ types/src/epoch.rs
@@ -61,31 +61,6 @@ pub struct EpochExtendedModel {
     pub total_validator_balance: u64,
 }
 
-#[derive(Serialize, Debug, Clone)]
-pub struct EpochExtendedView {
-    pub epoch: u64,
-    #[serde(flatten)]
-    pub model: EpochModel,
-    pub finalized: bool,
-    pub global_participation_rate: f64,
-    #[serde(flatten)]
-    pub extended_model: EpochExtendedModel,
-}
-
-impl From<(u64, EpochModel, EpochExtendedModel)> for EpochExtendedView {
-    fn from((epoch, model, extended_model): (u64, EpochModel, EpochExtendedModel)) -> Self {
-        let global_participation_rate = (model.voted_ether as f64).div(model.eligible_ether as f64);
-
-        EpochExtendedView {
-            epoch,
-            model,
-            finalized: global_participation_rate >= 2f64 / 3f64,
-            global_participation_rate,
-            extended_model,
-        }
-    }
-}
-
 #[cfg_attr(feature = "wasm", wasm_bindgen)]
 #[derive(Persistable, Serialize, Deserialize, Debug, Clone)]
 #[persistable(prefix = "/epochs/meta")]
