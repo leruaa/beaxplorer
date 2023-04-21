6bb689e01154f214c06a26466ac4f34f4805238e
aurel <aurelien.catinon@gmail.com>
2023-04-21 06:26:32 +0000

    Fix links

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -7,9 +7,9 @@ export default () => {
         <nav className="container mx-auto flex flex-row text-lg">
           <h1 className="text-xl py-2"><a href="/">Beacon explorer</a></h1>
           <ol className="flex flex-row flex-grow justify-end">
-            <li className="p-2"><Link href="/epochs"><a><i className="icon outline-clock" /> Epochs</a></Link></li>
-            <li className="p-2"><Link href="/blocks"><a><i className="icon outline-cube" /> Blocks</a></Link></li>
-            <li className="p-2"><Link href="/validators"><a><i className="icon outline-users" /> Validators</a></Link></li>
+            <li className="p-2"><Link href="/epochs"><i className="icon outline-clock" /> Epochs</Link></li>
+            <li className="p-2"><Link href="/blocks"><i className="icon outline-cube" /> Blocks</Link></li>
+            <li className="p-2"><Link href="/validators"><i className="icon outline-users" /> Validators</Link></li>
           </ol>
         </nav>
       </header>
