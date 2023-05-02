45acf778af5c33054d32bc87c8f20f6977321a82
aurel <aurelien.catinon@gmail.com>
2023-05-02 20:14:20 +0000

    Add Card component

---
+++ web/components/card.tsx
@@ -0,0 +1,26 @@
+import { ClockCountdown, Icon, IconContext } from '@phosphor-icons/react';
+import cx from 'classnames';
+
+type RootProps = { className?: string, title: string, icon: JSX.Element, children: JSX.Element | JSX.Element[] };
+
+export default ({ className, title, icon, children }: RootProps) => {
+
+  return (
+    <div className={cx(className, "relative flex flex-col gap-1 px-2 h-24 rounded text-white")}>
+      <div className="absolute -right-6 -top -bottom opacity-25">
+        <IconContext.Provider
+          value={{
+            size: "6em",
+            weight: "bold"
+          }}
+        >
+          {icon}
+        </IconContext.Provider>
+      </div>
+      <h3 className="uppercase opacity-50 font-bold">{title}</h3>
+      <div className="opacity-90">
+        {children}
+      </div>
+    </div>
+  )
+}
