847225b6c4f66ac0ac8f88278a44ec34a5effe1c
aurel <aurelien.catinon@gmail.com>
2023-05-02 20:23:05 +0000

    Add cards to epoch details

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -7,17 +7,17 @@ export default ({ className, title, icon, children }: RootProps) => {
 
   return (
     <div className={cx(className, "relative flex flex-col gap-1 px-2 h-24 rounded text-white")}>
-      <div className="absolute -right-6 -top -bottom opacity-25">
+      <div className="absolute -right-6 -top -bottom opacity-20">
         <IconContext.Provider
           value={{
             size: "6em",
-            weight: "bold"
+            weight: "regular"
           }}
         >
           {icon}
         </IconContext.Provider>
       </div>
-      <h3 className="uppercase opacity-50 font-bold">{title}</h3>
+      <h3 className="uppercase opacity-70 font-bold">{title}</h3>
       <div className="opacity-90">
         {children}
       </div>
--- web/pages/epoch/[epoch].tsx
+++ web/pages/epoch/[epoch].tsx
@@ -8,8 +8,12 @@ import RelativeDatetime from '../../components/relative-datetime';
 import BlocksTable from '../../components/blocks/blocks-table';
 import { useBuffer } from '../../hooks/data';
 import { Suspense } from 'react';
-import { ClockCountdown } from '@phosphor-icons/react';
+import Bdg from '../../components/badge';
+import Card from '../../components/card';
+import { Calendar, Certificate, ClockCountdown, IdentificationBadge, ListChecks, SealCheck, SealWarning } from '@phosphor-icons/react';
 import Link from 'next/link';
+import Datetime from '../../components/datetime';
+import { Root as Separator } from '@radix-ui/react-separator';
 
 
 export default (props) => {
@@ -55,33 +59,70 @@ const Epoch = ({ id }: { id: bigint }) => {
 
   return (
     <section className="container mx-auto">
-      <div className="tabular-data">
-        <p>Showing epoch</p>
+      <div className="grid grid-flow-row grid-cols-5 gap-2">
+        <Card
+          className="bg-gradient-to-r from-sky-500 to-blue-500"
+          title="Epoch"
+          icon={<ClockCountdown />}>
+          <span className="text-5xl font-semibold">{epoch.epoch}</span>
+        </Card>
 
-        <dl>
-          <dt>Epoch</dt>
-          <dd>{epoch.epoch}</dd>
-          <dt>Finalized</dt>
-          <dd>
-            {epoch.finalized
-              ? <span>Yes</span>
-              : <span>No</span>
-            }
-          </dd>
-          <dt>Time</dt>
-          <dd>
-            <RelativeDatetime timestamp={epoch.timestamp} />
-          </dd>
-          <dt>Attestations</dt>
-          <dd>{epoch.attestationsCount}</dd>
-          <dt>Deposits</dt>
-          <dd>{epoch.depositsCount}</dd>
-          <dt>Slashings P / A</dt>
-          <dd>{epoch.proposerSlashingsCount} / {epoch.attesterSlashingsCount}</dd>
-          <dt>Voting Participation</dt>
-          <dd><Ethers value={epoch.votedEther} /> of <Ethers value={epoch.eligibleEther} /> (<Percentage value={epoch.globalParticipationRate} />)</dd>
-        </dl>
+        <Card
+          className="bg-gradient-to-r from-green-500 to-emerald-500"
+          title="State"
+          icon={<Certificate />}>
+          <span className="text-4xl">
+            Finalized
+          </span>
+        </Card>
+        <Card
+          className="bg-gradient-to-r from-violet-500 to-purple-500"
+          title="Time"
+          icon={<Calendar />}>
+          <div className="text-3xl">
+            <RelativeDatetime timestamp={epoch.timestamp} /> ago
+          </div>
+          <div className="text-lg opacity-75">
+            <Datetime timestamp={epoch.timestamp} />
+          </div>
+        </Card>
+        <Card
+          className="bg-gradient-to-r from-green-500 to-emerald-500"
+          title="Proposed blocks count"
+          icon={<SealCheck />}>
+          <span className="text-5xl font-semibold">
+            {epoch.proposedBlocksCount}
+          </span>
+        </Card>
+        <Card
+          className="bg-gradient-to-r from-amber-500 to-yellow-500"
+          title="Missed blocks count"
+          icon={<SealWarning />}>
+          <span className="text-5xl font-semibold">
+            {epoch.missedBlocksCount}
+          </span>
+        </Card>
+        <Card
+          className="bg-gradient-to-r from-violet-500 to-purple-500"
+          title="Attestations"
+          icon={<ListChecks />}>
+          <div className="text-5xl font-semibold">
+            {epoch.attestationsCount}
+          </div>
+        </Card>
+        <Card
+          className="col-span-2 bg-gradient-to-r from-purple-500 to-fuchsia-500"
+          title="Voting participation"
+          icon={<IdentificationBadge />}>
+          <div className="text-3xl font-semibold">
+            <Ethers value={epoch.votedEther} /> of <Ethers value={epoch.eligibleEther} />&nbsp;ETH{" "}
+            (<Percentage value={epoch.globalParticipationRate} />)
+          </div>
+        </Card>
       </div>
+
+      <Separator className="my-5" />
+
       <div className="tabular-data">
         <BlocksTable app={app} blocksCount={32} kind={{ kind: "epoch", number: epoch.epoch }} />
       </div>
