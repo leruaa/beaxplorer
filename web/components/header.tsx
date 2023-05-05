166463267af3e778596543375af3af75f5266384
leruaa <aurelien.catinon@gmail.com>
2023-05-05 14:50:44 +0200

    Add header styles to component directly

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -10,13 +10,13 @@ export default () => {
         className: "inline mb-1",
       }}
     >
-      <header>
-        <nav className="container mx-auto flex flex-row text-lg">
-          <h1 className="text-xl py-2"><a href="/">Beacon explorer</a></h1>
+      <header className="p-1 bg-blue-900 text-blue-50">
+        <nav className="container mx-auto flex flex-row text-lg text-blue-50">
+          <h1 className="text-xl py-2"><Link className="text-blue-50" href="/">Beacon explorer</Link></h1>
           <ol className="flex flex-row flex-grow justify-end">
-            <li className="p-2"><Link href="/epochs"><ClockCountdown /> Epochs</Link></li>
-            <li className="p-2"><Link href="/blocks"><Cube /> Blocks</Link></li>
-            <li className="p-2"><Link href="/validators"><UsersThree /> Validators</Link></li>
+            <li className="p-2"><Link href="/epochs" className="text-blue-50"><ClockCountdown /> Epochs</Link></li>
+            <li className="p-2"><Link href="/blocks" className="text-blue-50"><Cube /> Blocks</Link></li>
+            <li className="p-2"><Link href="/validators" className="text-blue-50"><UsersThree /> Validators</Link></li>
           </ol>
         </nav>
       </header>
