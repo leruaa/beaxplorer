9b25bebc35491ecf839b2f3840f379707c629de2
aurel <aurelien.catinon@gmail.com>
2023-05-08 20:09:05 +0000

    Card icon is optional

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -1,22 +1,24 @@
 import { ClockCountdown, Icon, IconContext } from '@phosphor-icons/react';
 import cx from 'classnames';
 
-type RootProps = { className?: string, titleClassName?: string, contentClassName?: string, title: string, icon: JSX.Element, children: string | number | JSX.Element | JSX.Element[] };
+type RootProps = { className?: string, titleClassName?: string, contentClassName?: string, title: string, icon?: JSX.Element, children: string | number | JSX.Element | JSX.Element[] };
 
 export default ({ className, titleClassName, contentClassName, title, icon, children }: RootProps) => {
 
   return (
     <div className={cx(className, "relative flex flex-col gap-1 px-2 h-24 rounded text-white overflow-hidden")}>
-      <div className="absolute -right-6 -top -bottom opacity-30">
-        <IconContext.Provider
-          value={{
-            size: "6em",
-            weight: "regular"
-          }}
-        >
-          {icon}
-        </IconContext.Provider>
-      </div>
+      {icon &&
+        <div className="absolute -right-6 -top -bottom opacity-30">
+          <IconContext.Provider
+            value={{
+              size: "6em",
+              weight: "regular"
+            }}
+          >
+            {icon}
+          </IconContext.Provider>
+        </div>
+      }
       <h3 className={cx(titleClassName, "uppercase font-bold")}>{title}</h3>
       <div className={cx(contentClassName, "opacity-90")}>
         {children}
