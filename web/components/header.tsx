a0bb2ecd452423317c524b894fc8eb1dc670d048
leruaa <aurelien.catinon@gmail.com>
2023-05-30 10:23:54 +0200

    Use Radix for navigation menu

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -1,5 +1,6 @@
 import { ClockCountdown, Cube, IconContext, UsersThree } from '@phosphor-icons/react'
 import Link from 'next/link'
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
 export default () => {
   return (
@@ -10,16 +11,62 @@ export default () => {
         className: "inline mb-1",
       }}
     >
-      <header className="p-1 bg-blue-900 text-blue-50">
-        <nav className="container mx-auto flex flex-row text-lg text-blue-50">
-          <h1 className="text-xl py-2"><Link className="text-blue-50" href="/">Beacon explorer</Link></h1>
-          <ol className="flex flex-row flex-grow justify-end">
-            <li className="p-2"><Link href="/epochs" className="text-blue-50"><ClockCountdown /> Epochs</Link></li>
-            <li className="p-2"><Link href="/blocks" className="text-blue-50"><Cube /> Blocks</Link></li>
-            <li className="p-2"><Link href="/validators" className="text-blue-50"><UsersThree /> Validators</Link></li>
-          </ol>
-        </nav>
+      <header className="p-1 bg-gray-100 text-gray-800">
+        <NavigationMenu.Root>
+          <NavigationMenu.List asChild>
+            <nav className="container mx-auto flex flex-row text-lg">
+              <NavigationMenu.Item asChild>
+                <h1 className="text-xl py-2">
+                  <NavigationMenu.Link asChild>
+                    <Link href="/">Beacon explorer</Link>
+                  </NavigationMenu.Link>
+                </h1>
+              </NavigationMenu.Item>
+              <ol className="flex flex-row flex-grow justify-end">
+                <NavigationMenu.Item asChild>
+                  <li className="p-2">
+                    <NavigationMenu.Link asChild>
+                      <Link href="/epochs"><ClockCountdown /> Epochs</Link>
+                    </NavigationMenu.Link>
+                  </li>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item asChild>
+                  <li className="p-2">
+                    <NavigationMenu.Link asChild>
+                      <Link href="/blocks"><Cube /> Blocks</Link>
+                    </NavigationMenu.Link>
+                  </li>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item asChild>
+                  <li className="p-2 relative">
+                    <NavigationMenu.Trigger asChild>
+                      <NavigationMenu.Link asChild>
+                        <Link href="/validators"><UsersThree /> Validators</Link>
+                      </NavigationMenu.Link>
+                    </NavigationMenu.Trigger>
+                    <NavigationMenu.Content className="absolute top-10 left-0 bg-white rounded shadow">
+                      <NavigationMenu.Sub>
+                        <NavigationMenu.List asChild>
+                          <ol>
+                            <NavigationMenu.Item asChild>
+                              <li className="p-2">
+                                <NavigationMenu.Link asChild>
+                                  <Link href="/deposits">Deposits</Link>
+                                </NavigationMenu.Link>
+                              </li>
+                            </NavigationMenu.Item>
+                          </ol>
+                        </NavigationMenu.List>
+                      </NavigationMenu.Sub>
+                    </NavigationMenu.Content>
+
+                  </li>
+                </NavigationMenu.Item>
+              </ol>
+            </nav>
+          </NavigationMenu.List>
+        </NavigationMenu.Root>
       </header>
-    </IconContext.Provider>
+    </IconContext.Provider >
   )
 }
